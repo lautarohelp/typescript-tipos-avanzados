86873578177cd41a101136408ab8de74b35cdc17
lautarohelp <lautarotoledo391@gmail.com>
2024-01-22 16:57:11 -0600

    Propiedades de solo lectura

--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -3,5 +3,7 @@ import { Product, Sizes } from "./product.model";
 export const products: Product[] = [];
 
 export const addProduct = (data: Product) => {
+  // data.id = 'rftgyhjkl';
+  // data.createdAt = new Date(2004, 10, 7);
   products.push(data);
 }
