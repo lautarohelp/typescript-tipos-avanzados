1b4d80c488b7b940325048cf6dd29255f1a8eb9f
lautarohelp <lautarotoledo391@gmail.com>
2024-01-29 09:56:47 -0600

    Acceder al tipado por indice

--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -22,7 +22,7 @@ export const addProduct = (data: CreateProductDto): Product => {
   return newProduct;
 }
 
-export const updateProduct = (id: string, changes: UpdateProductDto): Product => {
+export const updateProduct = (id: Product['id'], changes: UpdateProductDto): Product => {
   const index = products.findIndex(item => item.id === id);
   const prevData = products[index];
   products[index] = {
